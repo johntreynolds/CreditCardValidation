7fe6d4c4d62f7834b4fda6f9a77a26214cb94c84
John Reynolds <john.reynolds@mullen.pvt.k12.co.us>
2026-03-05 12:47:49 -0700

    Added new arraylist handling for the math stuff. Started the calculations

--- src/ValidationRunner.java
+++ src/ValidationRunner.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class ValidationRunner
 	{
 		static Scanner scanner = new Scanner(System.in);
@@ -22,11 +22,11 @@ public class ValidationRunner
 					}
 			}
 		
-		public static void printNumber(String[] creditCardNumber)
+		public static void printNumber(ArrayList <Integer> creditCardNumber)
 			{
 				for (int i = 0; i < 16; i++)
 					{
-						System.out.print(creditCardNumber[i]);
+						System.out.print(creditCardNumber.get(i));
 					}
 			}
 
@@ -61,21 +61,30 @@ public class ValidationRunner
 				return choice;
 			}
 
-		public static String[] getCreditCardNumber()
+		public static ArrayList<Integer> getCreditCardNumber()
 			{
+				
 				System.out
 				.println("Give me a credit card number and I will tell you if it is potentially valid");
+				ArrayList <Integer> creditCardNumber = new ArrayList <Integer>();
 				while (true)
 					{
 						
 						System.out.println("Type the number with NO SPACES");
 						System.out.print("Type the number: ");
 						String input = scanner.nextLine();
-						String[] creditCardNumber = input.split("");
-						if (creditCardNumber.length == 16)
+						if (input.length() == 16)
 							{
+								
+								for (int i = 0; i < input.length(); i++)
+									{
+										int number = Integer.parseInt(input.substring(i, i + 1));  
+										creditCardNumber.add(0, number);
+									}
 								return creditCardNumber;
 							}
+						
+						
 						System.out.println("The number you entered is not in the proper format, please try again");
 						System.out.println("Check for any spaces or the length of the card number");
 					}
@@ -83,9 +92,25 @@ public class ValidationRunner
 
 			}
 
-		public static boolean numberValid(String[] creditCardNumber)
+		public static boolean numberValid(ArrayList<Integer> creditCardNumber)
 			{
 				boolean numberValid = false;
+				for (int i = 0; i < 16; i++)
+					{
+						if (i % 2 == 1)
+							{
+								creditCardNumber.set(i, creditCardNumber.get(i) * 2);
+							}
+						else
+							{
+								
+							}
+						System.out.print(creditCardNumber.get(i));
+					}
+				
+				
+				
+				
 				return numberValid;
 			}
 
